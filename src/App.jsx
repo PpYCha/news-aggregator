40eecc62d3eafb4104542c83c824d6767d47ca1d
PpYCha <diasandiaz7@gmail.com>
2023-06-29 20:31:03 +0800

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY;
+
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await fetch(
+          `https://newsapi.org/v2/top-headlines?country=us&apiKey=${NEWS_API_KEY}`
+        );
+        const data = await response.json();
+        setArticles(data.articles);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchArticles();
+  }, []);
+
+  return (
+    <div>
+      <h1>News Feed</h1>
+      {articles.map((article, index) => (
+        <div key={index}>
+          <h3>{article.title}</h3>
+          <p>{article.description}</p>
+          <img src={article.urlToImage} alt="Article" />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
